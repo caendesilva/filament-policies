2ea40fb2c23351f4951382ba556d8ea50addcfd7
Caen De Silva <caen@desilva.se>
2023-09-13 21:14:40 +0200

    Improve policy layout

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,5 +1,8 @@
 <x-filament-panels::layout.base :livewire="$livewire">
-    <div class="px-4 py-8">
+    <div class="px-4 py-8 grid auto-cols-fr gap-y-6">
+        <div class="mx-auto my-4">
+            <x-filament-panels::logo />
+        </div>
         <x-filament::section class="max-w-5xl mx-auto">
             <article @class([
                 'prose', 'dark:prose-invert' => \Filament\Facades\Filament::hasDarkMode()
@@ -8,5 +11,8 @@
                 {!! $policy !!}
             </article>
         </x-filament::section>
+        <footer class="mx-auto">
+            <x-filament::link :href="url('/')">Back home</x-filament::link>
+        </footer>
     </div>
 </x-filament-panels::layout.base>
