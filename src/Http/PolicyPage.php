7bb94767ae3332ed9e0cdd8b82552029fd415d4d
caendesilva <caendesilva@users.noreply.github.com>
2023-09-13 14:05:12 +0000

    Fix styling

--- src/Http/PolicyPage.php
+++ src/Http/PolicyPage.php
@@ -4,19 +4,19 @@ namespace Desilva\FilamentPolicies\Http;
 
 use Filament\Pages\BasePage;
 use Illuminate\Support\Str;
-use Livewire\Livewire;
 
 class PolicyPage extends BasePage
 {
     protected static string $view = 'filament-policies::policy';
 
     protected string $policy;
+
     protected string $file;
 
     public function __construct(string $policy)
     {
         $this->policy = $policy;
-        $this->file = resource_path('markdown/policies/'.basename($policy).'.md');
+        $this->file = resource_path('markdown/policies/' . basename($policy) . '.md');
 
         if (! file_exists($this->file)) {
             abort(404);
